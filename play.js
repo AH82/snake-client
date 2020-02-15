f1f84d991dd73a5bc4c4fcfb8b86131d42c79ea9
Hatem <hatem.deux@gmail.com>
2020-02-15 16:00:28 -0500

    SetupInput and handleUserInput functions moved to newly created input.js which exports them; play.js now requires input.js

--- play.js
+++ play.js
@@ -1,23 +1,12 @@
 // const net = require('net');
-const {connect} = require('./client')
+const { connect } = require('./client')
+const { setupInput } = require('./input');
+
 /**
  * Establishes connection with the game server
  */
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput);
-  stdin.resume();
-  return stdin;
-}
-
 
 console.log('Connecting ...');
 connect();
+
 setupInput();
